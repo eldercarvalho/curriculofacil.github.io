7961d3fd8f8668ffac03e36051db741d6ff3429a
Elder Carvalho <eld.carv@gmail.com>
2025-10-16 22:43:01 -0300

    feat: created site with react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="title">Currículo Fácil</h1>
+        <div className="subtitle">Crie seu currículo de forma simples e profissional</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
